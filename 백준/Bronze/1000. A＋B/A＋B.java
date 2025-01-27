7a50dbc76e972fe76f9af12d9b00b9ae5003b0be
song <89139633+ssongcookie@users.noreply.github.com>
2025-01-27 18:18:31 +0900

    [Bronze V] Title: A+B, Time: 180 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        int A;
+        int B;
+        
+        Scanner sc = new Scanner(System.in);
+        
+        A = sc.nextInt();
+        B = sc.nextInt();
+        
+        System.out.print(A + B);
+    }
+}
