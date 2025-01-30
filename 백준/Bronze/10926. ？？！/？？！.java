4d6e9c5a009625db9c6ef6ae47a35d0936031b47
song <89139633+ssongcookie@users.noreply.github.com>
2025-01-30 22:31:04 +0900

    수정: 사용자 ID 입력에 대한 불필요한 null 체크 제거 및 코드 간소화

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -2,12 +2,26 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
+        /**
+        * [AS-IS]
+        * 
+        * Scanner sc = new Scanner(System.in);
+        * String id = sc.next();
+        * 
+        * if(id != null){
+        *     System.out.print(id + "??!");
+        * }
+
+        /**
+        * [TO-BE]
+        * 1. 입력 체크 생략
+        * 2. 간결성
+        * 3. 자원 관리: sc.close()를 추가하여 자원을 해제합니다.
+        **/
+
         Scanner sc = new Scanner(System.in);
         String id = sc.next();
-        
-        if(id != null){
-            System.out.print(id + "??!");
-        }
-        
+        System.out.print(id + "??!");
+        sc.close();
     }
 }
