d61dfc33d5d8dc766b672063775faf368d2c60b1
song <89139633+ssongcookie@users.noreply.github.com>
2025-02-18 23:25:15 +0900

    [Bronze III] Title: 알람 시계, Time: 196 ms, Memory: 18508 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int H = sc.nextInt();
+        int M = sc.nextInt();
+        
+        if(M >= 45){
+            M = M - 45;
+        } else {
+            H = H - 1;
+            if(H < 0) {
+                H = 23;
+            }
+            M = M +60 -45;
+        }
+        
+        System.out.println(H + " " + M);
+    }
+}
